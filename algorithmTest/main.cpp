26e2814bd96fae16cd82fddbb7f651a6fb685680
nuokaqiao <sjh_qdu@163.com>
2018-05-16 21:05:40 +0800

    添加algorithmTest

---
+++ algorithmTest/main.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: sjh
+	> Mail: sjh_qdu@163.com 
+	> Created Time: 2018年05月14日 星期一 21时50分02秒
+ ************************************************************************/
+#include <iostream>
+#include <vector>
+#include "sort.h"
+using namespace std;
+
+int main()
+{
+	int A[8] = {6,5,3,1,8,7,2,4};
+	vector<int> arry(A,A+8);	
+	BubbleSort(arry);
+	std::cout<< "冒泡：";
+	for(vector<int>::iterator it = arry.begin(); it != arry.end(); ++it)
+		cout << *it;
+	cout << endl;
+	return 0;
+}
